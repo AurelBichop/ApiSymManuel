e902249cf76786dfc435731aead816c82f499c9e
Aurelien <linux.aurelien@gmail.com>
2020-01-13 18:55:37 +0100

    api manuel

---
+++ src/EventSubscriber/ExceptionSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class ExceptionSubscriber implements EventSubscriberInterface
+{
+    public function onExceptionEvent(ExceptionEvent $event)
+    {
+
+        $exception = $event->getThrowable();
+
+        if ($exception instanceof NotFoundHttpException) {
+            $data = [
+                'status' => $exception->getCode(),
+                'message' => 'Resource not found'
+            ];
+
+        $response = new JsonResponse($data);
+        $event->setResponse($response);
+        }
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return [
+            ExceptionEvent::class => 'onExceptionEvent',
+        ];
+    }
+}
